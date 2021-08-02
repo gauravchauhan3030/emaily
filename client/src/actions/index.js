8872187a7ad5b9667dcb7d109210188cbebb7552
Gaurav Chauhan <gauravchauhan@Gauravs-MacBook-Air.local>
2021-08-03 00:59:21 +0530

    integrated the front end

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FETCH_USER } from "./types";
+import { FETCH_USER, FETCH_SURVEY } from "./types";
 
 export const fetchUser = () => async (dispatch) => {
   const res = await axios.get("/api/current_user");
@@ -25,6 +25,14 @@ export const surveySubmit = (values, history) => async (dispatch) => {
 
   dispatch({
     type: FETCH_USER,
+    payload: res.data,
+  });
+};
+
+export const fetchSurvey = () => async (dispatch) => {
+  const res = await axios.get("/api/surveys");
+  dispatch({
+    type: FETCH_SURVEY,
     payload: res.data,
   });
 };
---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchSurvey } from "../../actions";
+
+const SurveyList = ({ fetchSurvey, surveys }) => {
+  useEffect(() => {
+    fetchSurvey();
+  }, []);
+
+  const renderSurveys = () => {
+    return surveys.reverse().map((survey) => {
+      return (
+        <div className="card darken-1" key={survey._id}>
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>{survey.body}</p>
+            <p className="right">
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+            <div className="card-action">
+              <a>Yes: {survey.yes}</a>
+              <a>No: {survey.no}</a>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+  return <div>{renderSurveys()}</div>;
+};
+
+function mapStateToProps({ surveys }) {
+  return { surveys };
+}
+
+export default connect(mapStateToProps, { fetchSurvey })(SurveyList);
